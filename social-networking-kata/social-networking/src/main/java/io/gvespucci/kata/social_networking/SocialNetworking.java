3abdd8176f6177190125b9fdaaf42d21ff8987cf
Giorgio Vespucci <giorgio.vespucci@gmail.com>
2017-09-29 21:49:40 +0200

    Give the project a Maven layout

---
+++ social-networking-kata/social-networking/src/main/java/io/gvespucci/kata/social_networking/SocialNetworking.java
@@ -0,0 +1,13 @@
+package io.gvespucci.kata.social_networking;
+
+/**
+ * Hello world!
+ *
+ */
+public class SocialNetworking 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
