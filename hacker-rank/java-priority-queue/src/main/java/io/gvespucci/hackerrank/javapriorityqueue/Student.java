1c590a9bb70fc5a8bfd04a163478ac5781ea1995
gvespucci <giorgio.vespucci@gmail.com>
2022-02-18 19:34:16 +0100

    Add test of static factory method

--- hacker-rank/java-priority-queue/src/main/java/io/gvespucci/hackerrank/javapriorityqueue/Student.java
+++ hacker-rank/java-priority-queue/src/main/java/io/gvespucci/hackerrank/javapriorityqueue/Student.java
@@ -1,19 +1,22 @@
 package io.gvespucci.hackerrank.javapriorityqueue;
 
+import java.nio.DoubleBuffer;
+
 public class Student {
 
     private final int id;
     private final String name;
     private final double cgpa;
 
-    Student(int id, String name, double cgpa) {
+    Student(String name, double cgpa, int id) {
         this.id = id;
         this.name = name;
         this.cgpa = cgpa;
     }
 
     static Student of(String event) {
-
+        String[] items = event.split(" ");
+        return new Student(items[0], Double.parseDouble(items[1]), Integer.parseInt(items[2]));
     }
 
     int getID() {
--- hacker-rank/java-priority-queue/src/test/java/io/gvespucci/hackerrank/javapriorityqueue/StudentTest.java
+++ hacker-rank/java-priority-queue/src/test/java/io/gvespucci/hackerrank/javapriorityqueue/StudentTest.java
@@ -2,6 +2,22 @@ package io.gvespucci.hackerrank.javapriorityqueue;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class StudentTest {
 
+    @Test
+    void shouldParseTheName() {
+        assertEquals("John", Student.of("John 3.75 50").getName());
+    }
+
+    @Test
+    void shouldParseTheCumulativeGradePointAverage() {
+        assertEquals(3.75, Student.of("John 3.75 50").getCGPA());
+    }
+
+    @Test
+    void shouldParseTheId() {
+        assertEquals(50, Student.of("John 3.75 50").getID());
+    }
 }
