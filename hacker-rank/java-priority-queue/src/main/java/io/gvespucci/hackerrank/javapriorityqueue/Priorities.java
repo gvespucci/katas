3255484aefddaa0b897b781e92000766856b60c5
gvespucci <giorgio.vespucci@gmail.com>
2022-02-19 17:49:34 +0100

    Extract the string command for readability

--- hacker-rank/java-priority-queue/src/main/java/io/gvespucci/hackerrank/javapriorityqueue/Priorities.java
+++ hacker-rank/java-priority-queue/src/main/java/io/gvespucci/hackerrank/javapriorityqueue/Priorities.java
@@ -13,12 +13,13 @@ public class Priorities {
     public List<Student> getStudents(final List<String> events) {
         for (final String event : events) {
             final String[] eventItems = event.split(" ");
-            if ("ENTER".equals(eventItems[0])) {
-                this.queue.offer(Student.of(event));
+            final String command = eventItems[0];
+            if (command.equals("ENTER")) {
+                this.queue.add(Student.of(event));
                 System.out.println("ENTER : " + Arrays.toString(this.queue.toArray()));
-            } else if ("SERVED".equals(eventItems[0])) {
-                this.queue.poll();
-                System.out.println("SERVED: " + Arrays.toString(this.queue.toArray()));
+            } else if (command.equals("SERVED")) {
+                System.out.println("REMOVING : " + this.queue.remove());
+                System.out.println("SERVED : " + Arrays.toString(this.queue.toArray()));
             }
         }
         return new ArrayList<>(this.queue);
